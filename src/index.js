a90f3229e6ff9cb085cd7bf1a9c764b3c2620f7f
KadsonLima <kadson-kaio@hotmail.com>
2022-07-02 00:40:12 -0300

    fix: PORT

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRouter from './routes/authRouter.js';
 import tradeRouter from "./routes/tradeRouter.js";
+import dotenv from 'dotenv';
 
 const server = express();
 server.use(cors(), express.json());
@@ -9,7 +10,8 @@ server.use(cors(), express.json());
 server.use(authRouter);
 server.use(tradeRouter);
 
+const PORT = process.env.PORT || 5000;
 
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log("RODANDO SERVER!!");
 });
